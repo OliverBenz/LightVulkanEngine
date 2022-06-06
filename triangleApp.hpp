b11a1a9ff53ab3dc554a3643455279428e372424
OliverBenz <oliver.benz@outlook.com>
2022-06-06 11:25:38 +0200

    Add staging buffer and device local vertex buffer.

--- triangleApp.hpp
+++ triangleApp.hpp
@@ -60,7 +60,11 @@ private:
 
     void createFramebuffers();
     void createCommandPool();
+    
     void createVertexBuffer();
+    void createBuffer(VkDeviceSize size, VkBufferUsageFlags usage, VkMemoryPropertyFlags properties, VkBuffer& buffer, VkDeviceMemory& bufferMemory);
+    void copyBuffer(VkBuffer srcBuffer, VkBuffer dstBuffer, VkDeviceSize size);
+
     uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
     void createCommandBuffers();
     void recordCommandBuffer(VkCommandBuffer commandBuffer, uint32_t imageIndex);
